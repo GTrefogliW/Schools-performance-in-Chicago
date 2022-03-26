0daa4af41c81c5c16c6841af0e5096a1d8147f72
GTrefogliW <81709039+GTrefogliW@users.noreply.github.com>
2022-03-25 20:07:17 -0500

    schools

---
+++ HW2 - Question 2.R
@@ -0,0 +1,55 @@
+library(sf)
+library(tidyverse)
+library(stringr)
+library(spData)
+library(scales)
+library(RColorBrewer)
+library(shiny)
+library(plotly)
+
+
+# Question 2
+path <- "C:/Users/guill/OneDrive/Documents/Data and Programming II/HW2/Boundaries - ZIP Codes"
+chi_shape <- st_read(file.path(path, "geo_export_fbe7a12f-9b8f-4ed9-b1eb-9b5cc7027af5.shp"))
+
+# choropleths for dataset 1 - Education
+
+df_educ <- read_csv("Chicago_Public_Schools_-_Progress_Report_Cards__2011-2012_.csv")
+# Merge
+df_educ$`ZIP Code` <- as.character(df_educ$`ZIP Code`)
+educ_il <- inner_join(chi_shape, df_educ, by = c("zip"="ZIP Code"))
+educ_il <- educ_il$`CPS Performance Policy Level`
+# Plot 1
+map_peformance <- ggplot() +
+  geom_sf(data = educ_il, aes(fill = `CPS Performance Policy Level`)) +
+  theme_minimal() +
+  labs(title = "Chicago Public Schools - Performance Level",
+       subtitle = "School Year 2011")
+
+map_peformance
+
+# choropleths for dataset 2 - Summer Program
+df_program <- read_csv("Libraries_-_2011_Children_s_Summer_Reading_Program.csv")
+# Merge
+df_program$`ZIP CODE` <- as.character(df_program$`ZIP CODE`)
+program_il <- inner_join(chi_shape, df_program, by = c("zip"="ZIP CODE"))
+# Plot 2
+map_programs <- ggplot() +
+  geom_sf(data = program_il, aes(fill = `2011 PARTICIPANTS`)) +
+  theme_minimal() +
+  scale_fill_gradient(low = "red", high = "green") +
+  labs(title = "Participation in The Chicago Public Library Summer Program",
+       subtitle = "Year 2011")
+
+map_programs
+
+# I found it interesting exploring these two dataset because they allow me to get initial
+# insights around the targeting criteria and coverage of educational interventions in the 
+# city Chicago. 
+
+# Based on Plot 1, it seems that the south side of the city is the one with the lowest 
+# education outcomes: their schools are ranked at Level 3 (lowest performance). In spite
+# of this fact, Plot 2 shows that the participation in the educational program run in 
+# the Summer of 2011 by The Chicago Public Library was the lowest for neighborhoods located
+# in that part of the city.
+
---
+++ app.R
@@ -0,0 +1,114 @@
+#  https://guillermotrefogli.shinyapps.io/Shiny_hw2/
+
+
+library(tidyverse)
+library(sf)
+library(tidyverse)
+library(stringr)
+library(spData)
+library(scales)
+library(RColorBrewer)
+library(shiny)
+library(plotly)
+# Question 3
+setwd("C:/Users/guill/OneDrive/Documents/Data and Programming II/HW2")
+df_educ_chi <- read_csv("educ_shi.csv")
+zip_list <- df_educ_chi$`ZIP Code`
+
+# shiny
+ui <- fluidPage(
+  fluidRow(
+    column(width = 3,
+           tags$img(src = "https://d11jve6usk2wa9.cloudfront.net/platform/10747/assets/logo.png",
+                    height = 90, width = 250)),
+    column(width = 4,
+           align = "center",
+           tags$h3("Performance Level of Chicago Public Schools - School Year 2011"),
+           tags$hr())
+  ),
+  fluidRow(
+    column(width = 12,
+           offset = 2,
+           align = "center",
+           checkboxInput(inputId = "bot",
+                         label = "Toggle Streets",
+                         value = FALSE))
+  ),
+  fluidRow(
+    column(width = 12,
+           align = "center",
+           plotlyOutput("street"))
+  ),
+  fluidRow(
+    column(width = 4,
+           align = "center",
+           selectizeInput(inputId = "zipcode",
+                          label = "Choose a zipcode to see more information",
+                          choices = zip_list))
+  ),
+  fluidRow(
+    column(width = 6,
+           plotlyOutput("plot")),
+     column(width = 6,
+           tableOutput("table"))
+  )
+  )
+
+server <- function(input, output) {
+  
+  df_educ_chi <- read_csv("educ_shi.csv")
+  shape_chi <- st_read("geo_export_fbe7a12f-9b8f-4ed9-b1eb-9b5cc7027af5.shp")
+  shape_chi$zip <- as.numeric(shape_chi$zip)
+  educ_shape_chi <- inner_join(df_educ_chi, shape_chi, by = c("ZIP Code" = "zip"))
+  
+  shape_sts <- st_read("Major_Streets.shp")
+  st_crs(shape_chi) == st_crs(shape_sts)
+  shape_chi <- st_transform(shape_chi, 4326)
+  shape_sts <- st_transform(shape_sts, 4326)
+  
+  output$street <- renderPlotly({
+    if (input$bot == FALSE) {
+      map_peformance <- ggplot() +
+        geom_sf(data = educ_shape_chi, aes(geometry = geometry,
+                                    fill = `CPS Performance Policy Level`)) +
+        theme_minimal() +
+        labs(title = "Overview of the Performance of Chicago Public Schools in 2011") +
+        theme(plot.title = element_text(hjust = 0.5))
+      map_peformance
+         
+    } else if (input$bot == TRUE) {
+      map_peformance <- ggplot() +
+        geom_sf(data = educ_shape_chi, aes(geometry = geometry,
+                                           fill = `CPS Performance Policy Level`)) +
+        geom_sf(data = shape_sts) +
+        theme_minimal() +
+        labs(title = "Overview of the Performance of Chicago Public Schools in 2011") +
+        theme(plot.title = element_text(hjust = 0.5))
+      map_peformance
+    }
+  })
+  
+  data_plot <- reactive({
+    dst <- df_educ_chi %>%
+      filter(`ZIP Code` == input$zipcode) %>% 
+      ggplot(aes(`CPS Performance Policy Level`)) +
+      geom_bar() +
+      labs(title = "Number of Schools by Performance Level")
+    ggplotly(dst)
+    })
+  
+  output$plot <- renderPlotly({data_plot()
+    })
+  
+  data_table <- reactive({df_educ_chi %>% 
+      filter(`ZIP Code` == input$zipcode) %>%
+      select(-`ZIP Code`) %>% 
+      rename("# Participants in Library Summer Program" = `2011 PARTICIPANTS`)
+  })
+  
+  output$table <- renderTable({data_table()
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
